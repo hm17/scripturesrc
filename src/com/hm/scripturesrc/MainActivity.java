9d65d53a2bd403748f194a174ef32f779ea12386
Hazel Madolid <hazel.madolid@gmail.com>
2016-07-17 17:29:58 -0500

    First commit

---
+++ src/com/hm/scripturesrc/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hm.scripturesrc;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
